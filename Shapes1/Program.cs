05e42e02501f4e811fcd65c17d1cdc7780ab369e
Ashley Likes <alikes1987@gmail.com>
2022-11-26 21:52:34 -0600

    Example assignment

---
+++ Shapes1/Program.cs
@@ -0,0 +1,54 @@
+﻿using Shapes;
+
+namespace Program {
+  static class Program {
+    public static void Main(string[] args) {
+      string filepath = args[0];
+      double total = 0;
+
+      foreach (string line in System.IO.File.ReadLines(filepath)) {
+        string[] lineData = line.Split(',');
+
+        Shape myShape;
+
+        switch (lineData[0]) {
+          case "circle":
+            myShape = new Circle(
+              Convert.ToDouble(lineData[2])
+            );
+          break;
+
+          case "rect":
+            myShape = new Rectangle(
+              Convert.ToDouble(lineData[2]),
+              Convert.ToDouble(lineData[3])
+            );
+          break;
+
+          case "square":
+            myShape = new Square(
+              Convert.ToDouble(lineData[2])
+            );
+          break;
+
+          default:
+            throw new FormatException($"Unable to parse shape from name {lineData[0]}");
+        } // switch
+
+
+        if (lineData[1] == "area") {
+          total += myShape.GetArea();
+        } else if (lineData[1] == "perim") {
+          total += myShape.GetPerimeter();
+        }
+
+
+
+
+
+      }
+
+      Console.WriteLine($"Your total measurements is {total:0,0.00}");
+    }
+  }
+}
