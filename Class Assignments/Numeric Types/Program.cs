7fc2bb91e918211599e2e0278bc3cb99409f3d63
Ashley Likes <alikes1987@gmail.com>
2023-03-20 17:03:33 -0500

    Restuarant assignment

--- Class Assignments/Numeric Types/Program.cs
+++ Class Assignments/Numeric Types/Program.cs
@@ -1,1 +1,26 @@
 ﻿using System;
+
+namespace NumericTypes
+{
+    class Rectangle
+    {
+        static void Main(string[]args)
+        {
+
+            Console.WriteLine("When prompted, enter the length and width of the rectangle to find the area. To finish, hit ENTER");
+            Console.WriteLine("What is the length of your rectangle?");
+            string length = Console.ReadLine();
+            int lengths = Int32.Parse(length);
+
+            Console.WriteLine("What is the width of your rectangle?");
+            string width = Console.ReadLine();
+            int widths = Int32.Parse(length);
+
+            int area = lengths * widths;
+            Console.WriteLine("The area of your rectangle is: " + area);
+        }
+          
+
+          
+        }
+    }
---
+++ Class Assignments/counting characters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class CountingCharacters {
+    public static void Main() {
+        string str = "This is a test string";
+        Console.WriteLine("String: " + str);
+        while (str.Length > 0) {
+            Console.Write(str[0] + " = ");
+            int cal = 0;
+            for (int j = 0; j <str.Length; j++) {
+                if (str[0] == str[j]) {
+                    cal ++;
+                }
+            }
+            Console.WriteLine(cal);
+            str = str.Replace(str[0].ToString(), string.Empty);
+        }
+        Console.ReadLine();
+    }
+}
