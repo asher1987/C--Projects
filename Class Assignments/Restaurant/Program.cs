e556644c7a4d249eb824cd67ad75927444ab46e4
Ashley Likes <alikes1987@gmail.com>
2023-03-21 10:32:13 -0500

    Restaurant assignment

--- Class Assignments/Restaurant/Program.cs
+++ Class Assignments/Restaurant/Program.cs
@@ -42,4 +42,66 @@ public class Menu
      
 
     }
+
+    public void RemoveItem(MenuItem item)
+    {
+        Items.Remove(item); // this line removes MenuItem object 
+        lastUPdated = DateTime.Now; // this line is used to update the 'LastUpdated' property every time something is removed
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Menu menu = new Menu(); 
+
+        MenuItem item1 = new MenuItem
+        {
+            Name = "Parisien Cheese",
+            Price = 13.00,
+            Description = "Warm ricotta cheese, Nicoise olives, tomato confit, olive oil and fresh herbs. Served with toasted crostini",
+            Category = "Appetizer",
+            IsNew = false
+        };
+
+        menu.AddItem(item1);
+
+        MenuItem item2 = new MenuItem
+        {
+            Name = "Salmon",
+            Price = 28.00,
+            Description = "Potato encrusted salmon topped iwth lenon beurre blanc. Served with haricot verts and garnish with arugula salad",
+            Category = "Entrees",
+            IsNew = true
+        };
+
+        menu.AddItem(item2);
+
+         MenuItem item3 = new MenuItem
+        {
+            Name = "Chocolate Mousse Cake",
+            Price = 10.00,
+            Description = "It's just cake man",
+            IsNew = false
+        };
+
+        menu.AddItem(item3);
+
+        //Display the menu
+        Console.WriteLine("Menu (last updated {0}):)", menu.lastUPdated);
+        foreach (MenuItem item in menu.Items)
+        {
+            Console.WriteLine("{0} ({1}): {2}", item.Name, item.Price.ToString("C"), item.Description);
+            Console.WriteLine("Category: {0}", item.Category);
+            if(item.IsNew)
+            {
+                Console.WriteLine("This is a new item!");
+
+            }
+            Console.WriteLine();
+        }
+
+        
+    }
 }
