f2720de0d2cc50a697eddab1c1dd8c9c7a282a7f
Ashley Likes <alikes1987@gmail.com>
2023-03-21 10:33:25 -0500

    commit

---
+++ Class Practice/HelloMethod1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+public class HelloWord1
+{
+    private string message = "Hello World";
+
+    public HelloWord1(string message)
+    {
+        this.message = message;
+    }
+    public void SayHello()
+    {
+        Console.WriteLine(message);
+    }
+
+
+HelloWord1 goodbye = new HelloWord1("goodbye");
+goodbye.SayHello(); 
+
+
+}
---
+++ Class Practice/Tempexample/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Temperature
+{
+    private double fahrenheit;
+
+    public double Fahrenheit
+    {
+       get 
+       {
+        return fahrenheit;
+       }
+       set
+       {
+        double absolutezeroFahreneit = -459.67;
+        if (value < absolutezeroFahreneit)
+        {
+            throw new ArgumentException("Value is below zero");
+        }
+        fahrenheit = value;
+       }
+    }
+}
