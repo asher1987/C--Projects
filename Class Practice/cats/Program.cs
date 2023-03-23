c43444d2e2f1cde2f1a8b83d9bbe5c70d5797b37
Ashley Likes <alikes1987@gmail.com>
2023-03-23 11:07:21 -0500

    cat inheritance

--- Class Practice/cats/Program.cs
+++ Class Practice/cats/Program.cs
@@ -7,8 +7,14 @@ namespace Cats
         static void Main(string[]args)
         {
             //Try out your skills with inheritance here
-            HouseCat spike = new HouseCat("Spike");
-            Console.Write(spike.Weight);
+            HouseCat spike = new HouseCat("Spike", 12.0);
+            spike.Eat();
+            Console.WriteLine(spike.Tired);
+            Console.WriteLine(spike.Name);
+            Console.WriteLine("The name of the cat is " + spike.Name + ". Is the cat tired? " + spike.Tired );
+
+            HouseCat spot = new HouseCat("Spot", 12);
+            Console.WriteLine("The name of the other cat is " + spot.Name + " and she weighs " + spot.Weight + " pounds.");
         }
     }
 }
